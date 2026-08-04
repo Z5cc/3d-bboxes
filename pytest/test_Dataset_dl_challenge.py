670de8e206ef8abe97d275ae4c29d3d44e32948b
david <d.ncklsr@gmail.com>
2026-08-04 16:36:55 +0200

    handle missing data on server

--- pytest/test_Dataset_dl_challenge.py
+++ pytest/test_Dataset_dl_challenge.py
@@ -1,9 +1,14 @@
+import os
+import pytest
+
 from Dataset_dl_challenge import Dataset_dl_challenge
 from Constants import TEST_PATH
 
 
 
 def test_dataset():
+    if not os.path.exists(TEST_PATH):
+        pytest.skip("Test dataset is not available")
     dataset = Dataset_dl_challenge(TEST_PATH)
     # access first element
     x,y = dataset[0]
--- pytest/test_Model.py
+++ pytest/test_Model.py
@@ -1,9 +1,16 @@
+import os
+
+import pytest
 import torch
 
 from Model import Model
+from Constants import TEST_PATH
 
 
 def test_inference(tmp_path):
+    if not os.path.exists(TEST_PATH):
+        pytest.skip("Test dataset is not available")
+
     model = Model()
 
     model_path = tmp_path / "model.pth"
